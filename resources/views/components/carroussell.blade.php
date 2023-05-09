0376f2f2a97b5ef6edd9ca783c8af264bfdbe496
Daniel Valcarce <dvalcarce93@gmail.com>
2023-05-09 23:40:26 +0200

    Ruta y frontend de la vista index

---
+++ resources/views/components/carroussell.blade.php
@@ -0,0 +1,44 @@
+<div
+    id="carouselExampleIndicators"
+    class="carousel slide"
+    data-bs-ride="true"
+  >
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="2000">
+        <img src="img/leopard1425x800.jpg" class="img-fluid" height="800" width="1425" alt="leopard"/>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="img/tiger1425x800.jpg" class="img-fluid" height="800" width="1425" alt="tiger" />
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="img/ume1425x800.jpg" class="img-fluid" height="800" width="1425" alt="UME" />
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="img/czm1425x800.jpg" class="img-fluid" height="800" width="1425" alt="CZM" />
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="img/millan1425x800.jpg" class="img-fluid" height="800" width="1425" alt="Millan" />
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="img/paraca1425x800.jpg" class="img-fluid" height="800" width="1425" alt="paraca" />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,5 +30,118 @@
                     </div>
                 </nav>
             @endif
+            <div class="d-flex justify-content-center bg-warning">
+                <x-carroussell></x-carroussell>
+            </div>
+            <section class="py-5 border-bottom" id="features">
+                <h3 class="d-flex justify-content-center fw-folder">Tecnología y los mejores test de oposiciones para conseguir tu plaza de Cabo</h3>
+                <div class="container px-5 my-5">
+                    <div class="row gx-5">
+                        <div class="card col-lg-4 mb-5 mb-lg-0 hover bg-white px-5">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
+                            <h2 class="h4 fw-bolder">Tecnología</h2>
+                            <p>Nuestro software basado en Inteligencia Artificial (IA) aprende del alumno mediante machine learning y diseña el itinerario de aprendizaje más eficaz para optimizar su estudio reduciendo tiempo y esfuerzo.</p>
+                        </div>
+                        <div class="card col-lg-4 mb-5 mb-lg-0 hover bg-white">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
+                            <h2 class="h4 fw-bolder">Método</h2>
+                            <p>Más de 1.000 preguntas y test de diferentes bloques de estudio, actualizadas y revisadas a diario por un equipo editorial. Exámenes oficiales y simulacros para ensayar el día de la prueba y un sistema de evaluación para compararte en tiempo real con miles de opositores.</p>
+                        </div>
+                        <div class="card col-lg-4 hover bg-white">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                            <h2 class="h4 fw-bolder">Comunidad</h2>
+                            <p>Nuestra amplia comunidad de usuarios nos permite realizar estadisticas continuas y actualizadas por destino, para que sepas en todo momento tu situación con respecto a tus competidores</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="py-5 border-bottom bg-dark">
+                <div class="container px-5 my-5">
+                    <div class="text-center mb-5 text-light">
+                        <h2 class="fw-bolder">PAGA MENSUALMENTE</h2>
+                        <p class="lead mb-0">Sin Matricula</p>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <!-- Pricing card free-->
+                        <div class="col-lg-6 col-xl-4">
+                            <div class="card mb-5 mb-xl-0 hover">
+                                <div class="card-body p-5">
+                                    <div class="mb-3">
+                                        <span class="display-4 fw-bold">5€</span>
+                                        <span class="text-muted">/ mes</span>
+                                    </div>
+                                    <ul class="list-unstyled mb-4">
+                                        <li class="mb-2">
+                                            <i class="bi bi-check text-primary"></i>
+                                            <strong>1 usuario</strong>
+                                        </li>
+                                        <li class="mb-2">
+                                            <i class="bi bi-check text-primary"></i>
+                                            Examenes ilimitados
+                                        </li>
+                                        <li class="mb-2">
+                                            <i class="bi bi-check text-primary"></i>
+                                            Más de 1000 preguntas
+                                        </li>
+                                        <li class="mb-2">
+                                            <i class="bi bi-check text-primary"></i>
+                                            Compara resultados
+                                        </li>
+                                        <li class="mb-2 text-muted">
+                                            <i class="bi bi-x"></i>
+                                            Estadisticas nacionales
+                                        </li>
+                                        <li class="mb-2 text-muted">
+                                            <i class="bi bi-x"></i>
+                                            Estadisticas por destino
+                                        </li>
+                                        <li class="mb-2 text-muted">
+                                            <i class="bi bi-x"></i>
+                                            Soporte continuo
+                                        </li>
+                                    </ul>
+                                    <div class="d-grid"><a class="btn btn-outline-danger" href="#!">Registrarse</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="py-5 border-bottom">
+                <div class="container px-5 my-5">
+                    <div class="text-center mb-5">
+                        <h2 class="fw-bolder">Testimonios de nuestros clientes</h2>
+                        <p class="lead mb-0">A nuestros opositores les encanta</p>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-6">
+                            <!-- Testimonial 1-->
+                            <div class="card mb-4 hover">
+                                <div class="card-body p-4">
+                                    <div class="d-flex">
+                                        <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
+                                        <div class="ms-4">
+                                            <p class="mb-1">Los test son practicamente iguales a los del examen oficial, me ha ayudado mucho, con tan poco tiempo para preparar el examen esto es esencial si quieres aprobar</p>
+                                            <div class="small text-muted">- Javier Lago, A Coruña</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Testimonial 2-->
+                            <div class="card hover">
+                                <div class="card-body p-4">
+                                    <div class="d-flex">
+                                        <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
+                                        <div class="ms-4">
+                                            <p class="mb-1">Es un apoyo fantástico! Las preguntas están bien redactadas y actualizadas al temario actual.</p>
+                                            <div class="small text-muted">- Mario Tatay, Barcelona</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
     </body>
 </html>
