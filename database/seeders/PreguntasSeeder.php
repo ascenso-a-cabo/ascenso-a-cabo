d72b1e12cdacc9d85a4bb0924352c9804e2d699c
Daniel Valcarce <dvalcarce93@gmail.com>
2023-05-24 23:16:37 +0200

    Seeder hasta tema 4 completo

--- database/seeders/PreguntasSeeder.php
+++ database/seeders/PreguntasSeeder.php
@@ -142,7 +142,173 @@ class PreguntasSeeder extends Seeder
             ['id' => 94, 'enunciado' => 'Seguridad en las FAS. O.M. 50/2011. Art. 82. Composición de una guardia de
             seguridad.', 'tema_id' => 2],
             ['id' => 95, 'enunciado' => 'Seguridad en las FAS. O.M. 50/2011 ART.86. Cabo de guardia.', 'tema_id' => 2],
-            ['id' => 96, 'enunciado' => '', 'tema_id' => 3],
+            ['id' => 96, 'enunciado' => 'Mando de régimen interior. La acción del mando.', 'tema_id' => 3],
+            ['id' => 97, 'enunciado' => 'Mando de régimen interior. Régimen interior.', 'tema_id' => 3],
+            ['id' => 98, 'enunciado' => 'Mando de régimen interior. Se denomina unidad…', 'tema_id' => 3],
+            ['id' => 99, 'enunciado' => 'Mando de régimen interior. El mando se podrá ejercer…', 'tema_id' => 3],
+            ['id' => 100, 'enunciado' => 'Mando de régimen interior. El E.T. es una organización …', 'tema_id' => 3],
+            ['id' => 101, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 2. COMPLETAR.
+            DEFINICIONES. _______ , acción directiva especifica de las Fas, se refiere
+            al ejercicio de la autoridad, con la consiguiente responsabilidad, que
+            corresponde al militar en razón de su cargo, destino o servicio.', 'tema_id' => 3],
+            ['id' => 102, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 4. El E.T. está
+            constituido por…', 'tema_id' => 3],
+            ['id' => 103, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 5. Unidades, Centros y
+            Organismos…', 'tema_id' => 3],
+            ['id' => 104, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 6. El libro de
+            organización…', 'tema_id' => 3],
+            ['id' => 105, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 7. Bases,
+            Acuartelamiento y Establecimientos.', 'tema_id' => 3],
+            ['id' => 106, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 8. Autoridad y acción
+            de mando.', 'tema_id' => 3],
+            ['id' => 107, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 10. Carácter del mando.', 'tema_id' => 3],
+            ['id' => 108, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 12. Sucesión de
+            mando.', 'tema_id' => 3],
+            ['id' => 109, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 31. régimen interior', 'tema_id' => 3],
+            ['id' => 110, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 32. El libro de normas
+            de régimen interior.', 'tema_id' => 3],
+            ['id' => 111, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 34. Los actos de
+            régimen interior.', 'tema_id' => 3],
+            ['id' => 112, 'enunciado' => 'Señalar la falsa. Mando de régimen interior. OM 50/2011 Art. 51. La unidad de
+            servicios.', 'tema_id' => 3],
+            ['id' => 113, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 52. Los servicios e la
+            instalación.', 'tema_id' => 3],
+            ['id' => 114, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 53. Clases de servicios,
+            en general, una instalación podrá contar con los siguientes servicios:', 'tema_id' => 3],
+            ['id' => 115, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 78. Las guardias.
+            Conceptos generales.', 'tema_id' => 3],
+            ['id' => 116, 'enunciado' => 'Señala la falsa. Mando de régimen interior. OM 50/2011 Art. 79. Clases y carácter
+            de las guardias', 'tema_id' => 3],
+            ['id' => 117, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 90. Guardias de orden.', 'tema_id' => 3],
+            ['id' => 118, 'enunciado' => 'Señala la falsa. Mando de régimen interior. OM 50/2011 Art. 91. Composición de las
+            guardias de orden. En el caso más completo, las guardias de orden
+            podrán tener la siguiente composición…', 'tema_id' => 3],
+            ['id' => 119, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 95. Personal de Tropa
+            de Cuartel.', 'tema_id' => 3],
+            ['id' => 120, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 97. Cabo de cuartel,
+            cuarteleros e imaginarias.', 'tema_id' => 3],
+            ['id' => 121, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 98. Guardia de los
+            servicios.', 'tema_id' => 3],
+            ['id' => 122, 'enunciado' => 'Señala la falsa. Mando de régimen interior. OM 50/2011 Art. 105. Los turnos de las
+            guardias.', 'tema_id' => 3],
+            ['id' => 123, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 106. Nombramiento de
+            las guardias.', 'tema_id' => 3],
+            ['id' => 124, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 107. Prestación de las
+            guardias.', 'tema_id' => 3],
+            ['id' => 125, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 104. Organización de
+            las guardias. Quien no realizara guardias?', 'tema_id' => 3],
+            ['id' => 126, 'enunciado' => 'Mando de régimen interior. OM 50/2011 Art. 106. Nombramiento de
+            las guardias.', 'tema_id' => 3],
+            ['id' => 127, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 1. Objeto y ámbito de
+            aplicación.', 'tema_id' => 4],
+            ['id' => 128, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 1. Objeto y ámbito de
+            aplicación.', 'tema_id' => 4],
+            ['id' => 129, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 1. Objeto y ámbito de
+            aplicación de la ley. Esta ley es de aplicación a todos los miembros de las
+            FAS.', 'tema_id' => 4],
+            ['id' => 130, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 3. Vinculación con
+            las FAS.', 'tema_id' => 4],
+            ['id' => 131, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 3. Vinculación con
+            las FAS.', 'tema_id' => 4],
+            ['id' => 132, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 3. Vinculación con las
+            Fuerzas Armadas.', 'tema_id' => 4],
+            ['id' => 133, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 6. Modalidades de relación de
+            servicios.', 'tema_id' => 4],
+            ['id' => 134, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 7. Juramento o
+            promesa ante la bandera de España.', 'tema_id' => 4],
+            ['id' => 135, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 8. Renovaciones de
+            compromiso.', 'tema_id' => 4],
+            ['id' => 136, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 9. Compromiso de larga
+            duración.', 'tema_id' => 4],
+            ['id' => 137, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 10. Finalización y resolución
+            del compromiso de larga duración.', 'tema_id' => 4],
+            ['id' => 138, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 8/2006. Art. 14. ¿Bajo qué causas se podrá
+            cambiar de especialidad?', 'tema_id' => 4],
+            ['id' => 139, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 15. Promoción interna.', 'tema_id' => 4],
+            ['id' => 140, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 16. Se les facilitara de forma
+            prioritaria formación en aéreas relativas a las tecnologías de la
+            información y las comunicaciones, prevención de riesgos laborales,
+            medio ambiente, así como cursos para la obtención de los permisos de
+            conducción y aquellos otros que se consideren de interés para su
+            desarrollo profesional.', 'tema_id' => 4],
+            ['id' => 141, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 17. Juramento
+            promesa ante la bandera de España.', 'tema_id' => 4],
+            ['id' => 142, 'enunciado' => 'Legislación militar. Ley 8/2006. Art.17. definición y condiciones.', 'tema_id' => 4],
+            ['id' => 143, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 18. Activación e incorporación.', 'tema_id' => 4],
+            ['id' => 144, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 19. Funciones.', 'tema_id' => 4],
+            ['id' => 145, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 19. Funciones. La
+            acción del mando.', 'tema_id' => 4],
+            ['id' => 146, 'enunciado' => 'Legislación militar. Ley de carrera militar. Art. 19. Funciones. El
+            militar profesional ejerce funciones…', 'tema_id' => 4],
+            ['id' => 147, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 19. Funciones. Acción
+            directiva.', 'tema_id' => 4],
+            ['id' => 148, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 20. El tiempo de servicios
+            prestados en las FAS se considera como merito en los sistemas de
+            selección para el acceso a los cuerpos, escalas, plazas de funcionario y
+            puestos de carácter laboral de las administraciones públicas.', 'tema_id' => 4],
+            ['id' => 149, 'enunciado' => 'Legislación militar. Ley 8/2006. Art. 21. La secretaria del gobierno
+            gestionara y convendrá con instituciones públicas y entidades privadas
+            acciones orientadas a la incorporación laboral de los militares
+            profesionales de tropa y marinería.', 'tema_id' => 4],
+            ['id' => 150, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 23. Facultades y antigüedad
+            en el empleo militar:', 'tema_id' => 4],
+            ['id' => 151, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 24. Empleos con carácter
+            honorifico.', 'tema_id' => 4],
+            ['id' => 152, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 25. Cuerpos y escalas.', 'tema_id' => 4],
+            ['id' => 153, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 26. Cuerpos militares.
+            Cuerpos específicos del E.T.', 'tema_id' => 4],
+            ['id' => 154, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 26. Cuerpos militares.
+            Cuerpos comunes de las FAS.', 'tema_id' => 4],
+            ['id' => 155, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 41. Especialidades', 'tema_id' => 4],
+            ['id' => 156, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 42. Capacidades
+            profesionales. Se determinan por…', 'tema_id' => 4],
+            ['id' => 157, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 43. Enseñanza en las FAS.', 'tema_id' => 4],
+            ['id' => 158, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 47.enseñanza de formación de
+            tropa y marinería.', 'tema_id' => 4],
+            ['id' => 159, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 48. Enseñanza de
+            perfeccionamiento.', 'tema_id' => 4],
+            ['id' => 160, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 62. Cambio de escala.', 'tema_id' => 4],
+            ['id' => 161, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 75. Desarrollo de la carrera,
+            carrera militar de las escalas de tropa y marinería comenzaran con un
+            compromiso inicial, renovable hasta un máximo de 6 años.
+            Posteriormente, podrán suscribir un compromiso de larga duración hasta
+            cumplir los 45 años de edad.', 'tema_id' => 4],
+            ['id' => 162, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 76. Adquisición de la
+            condición de militar de carrera.', 'tema_id' => 4],
+            ['id' => 163, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 78. La condición de militar de
+            tropa y marinería se adquiere una vez incorporado al centro docente
+            militar correspondiente.', 'tema_id' => 4],
+            ['id' => 164, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 88. Régimen y sistema de
+            ascensos.', 'tema_id' => 4],
+            ['id' => 165, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 89. Ascenso a los diferentes
+            empleos. El ascenso a cabo mayor se hará por el sistema de concurso.', 'tema_id' => 4],
+            ['id' => 166, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 90. Condiciones para el
+            ascenso.', 'tema_id' => 4],
+            ['id' => 167, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 100. Clasificación de los
+            destinos.', 'tema_id' => 4],
+            ['id' => 168, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 108. Reglamentariamente se
+            determinara el tiempo que podrán permanecer en situación de servicio activo los prisioneros y desaparecidos, teniendo en cuenta lo establecido
+            en el Código Penal Militar.', 'tema_id' => 4],
+            ['id' => 169, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 109. Situación de servicios
+            especiales.', 'tema_id' => 4],
+            ['id' => 170, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 110. Situación de excedencia.', 'tema_id' => 4],
+            ['id' => 171, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 114. Pase retiro.', 'tema_id' => 4],
+            ['id' => 172, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 116. Con la perdida de la
+            condición de militar de carrera se dejara de estar sujeto al régimen
+            general de derechos y deberes de los miembros de las FAS y a las leyes
+            penales y disciplinarias militares.', 'tema_id' => 4],
+            ['id' => 173, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 117. Renuncia a la condición
+            de militar de carrera.', 'tema_id' => 4],
+            ['id' => 174, 'enunciado' => 'Señala la falsa. Legislación militar. Ley 39/2007. Art. 118. Finalización y resolución
+            de compromisos de los militares con una relación de servicios
+            profesionales de carácter temporal.
+            (Señalar la falsa).', 'tema_id' => 4],
+            ['id' => 175, 'enunciado' => 'Legislación militar. Ley 39/2007. Art. 120. A los militares
+            profesionales que, como resultado de los reconocimientos médicos o de
+            las pruebas psicológicas y físicas, se establezcan una incapacidad que
+            conlleve una limitación ocupar determinados destinos, se les garantizara
+            el principio de igualdad de trato en los destinos a los que puedan
+            acceder.', 'tema_id' => 4],
         ]);
     }
 }
